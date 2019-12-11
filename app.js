86a00fbd44e1f2f01511ab582a496dadd243ce40
Alaeddine <ia_nasri@esi.dz>
2019-12-11 09:22:14 +0100

    Translition All and end of the project

--- app.js
+++ app.js
@@ -8,4 +8,7 @@ const t1 = new TimelineMax();
 
 t1.fromTo(Hero,1,{height : "0%"},{height : "90%",ease: Power2.easeInOut})
 .fromTo(Hero,1.2,{width : "100%"},{width : "80%",ease: Power2.easeInOut})
-.fromTo(slider,1.2,{x: '-100%',},{x: '0%',ease: Power2.easeInOut})
+.fromTo(slider,1.2,{x: '-100%',},{x: '0%',ease: Power2.easeInOut},"-=1.2")
+.fromTo(logo,0.5,{opacity:0,x:30},{opacity:1,x: 0},"-=0.5")
+.fromTo(Jhin,0.5,{opacity:0,x:30},{opacity:1,x: 0},"-=0.5")
+.fromTo(headline,0.5,{opacity:0,x:30},{opacity:1,x: 0},"-=0.5");
